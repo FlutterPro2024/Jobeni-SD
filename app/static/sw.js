9c7da76a93f93df3ad9eee7471d363beed7e5e87
FlutterPro2024 <mohammedu3615@gmail.com>
2026-01-06 10:34:20 +0200

    Force PWA install prompt

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,16 @@
-self.addEventListener('install', (e) => {
-  console.log('Jobeni Service Worker Installed');
+const CACHE_NAME = 'jobeni-v2';
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/']);
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
